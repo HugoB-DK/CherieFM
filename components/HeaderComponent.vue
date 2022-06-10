4198af3cae175ad7197f4fde6a48577252ef1667
Alban <goupilalban@gmail.com>
2022-06-10 12:11:47 +0200

    push des modifs de la home

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,15 +1,14 @@
 <template>
   <header class="w-10/12 mt-5 mx-auto">
     <div class="flex justify-between">
-      <a href=""><img class="w-3/12 h-auto " src="~/assets/Logo.png"></a>
+      <a href="/"><img class="w-3/12 h-auto " src="~/assets/Logo.png"></a>
       <div class="flex justify-end gap-4">
         <img class="w-1/12 h-auto" src="~assets/Heart.svg">
         <img class="w-1/12 h-auto cast" src="~assets/cast_google.svg">
         <img
           class="w-1/12 h-auto"
           src="~assets/download_rouge.svg"
-          @click=" this.deferredPrompt.prompt()
-          "
+          @onClick="promptInstall"
         >
       </div>
     </div>
@@ -19,9 +18,16 @@
   </header>
 </template>
 <script>
+import { BeforeInstallPromptEvent } from 'vue-pwa-install'
+
 export default {
-  name: 'HeaderComponent'
+  name: 'HeaderComponent',
+  deferredPrompt: BeforeInstallPromptEvent,
+  promptInstall () {
+    this.deferredPrompt.prompt()
+  }
 }
+
 </script>
 <style>
 #__layout{
